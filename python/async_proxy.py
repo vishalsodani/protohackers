be494407ab54ce083dedb997cf1e495b29c53bd2
vishalsodani <vishalsodani@rediffmail.com>
2022-11-04 10:33:00 +0530

    refactor

--- python/async_proxy.py
+++ python/async_proxy.py
@@ -8,14 +8,14 @@ tony_address = '7YWHMfk9JZe0LM0g1ZauHuiSxhI'
 
 class ServerState:
 
-    async def add_client(self, reader: StreamReader, writer: StreamWriter): #A
-        asyncio.create_task(self._echo(reader, writer))
+    async def add_client(self, reader: StreamReader, writer: StreamWriter):
+        asyncio.create_task(self._client(reader, writer))
 
-    async def _echo(self, reader: StreamReader, writer: StreamWriter): #C
+    async def _client(self, reader: StreamReader, writer: StreamWriter):
         try:
             host = "chat.protohackers.com"
             stream_reader, stream_writer = await asyncio.open_connection(host, 16963)
-            asyncio.create_task(self._echoback(stream_reader, writer))
+            asyncio.create_task(self._upstream(stream_reader, writer))
             while (data := await reader.readline()):
                 tony = re.sub(r"(^|(?<= ))7[a-zA-Z0-9]{25,34}($|(?= ))", tony_address, data.decode("utf-8"))
                 stream_writer.write(bytes(tony, "utf-8"))
@@ -28,7 +28,7 @@ class ServerState:
             traceback.print_exc()
 
 
-    async def _echoback(self, reader: StreamReader, writer: StreamWriter): #C
+    async def _upstream(self, reader: StreamReader, writer: StreamWriter):
         try:
             while (data := await reader.readline()):
                 tony = re.sub(r"(^|(?<= ))7[a-zA-Z0-9]{25,34}($|(?= ))", tony_address, data.decode("utf-8"))
@@ -45,10 +45,10 @@ class ServerState:
 async def main():
     server_state = ServerState()
 
-    async def client_connected(reader: StreamReader, writer: StreamWriter) -> None: #E
+    async def client_connected(reader: StreamReader, writer: StreamWriter) -> None:
         await server_state.add_client(reader, writer)
 
-    server = await asyncio.start_server(client_connected, '127.0.0.1', 5003) #F
+    server = await asyncio.start_server(client_connected, '127.0.0.1', 5003)
 
     async with server:
         await server.serve_forever()
