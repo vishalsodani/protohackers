86779a8a42b403d52bb27fb8c31cffa847b0bc6f
vishalsodani <vishalsodani@rediffmail.com>
2023-01-02 20:44:01 +0530

    working version which passed 1priorities.test

--- python/job_center.py
+++ python/job_center.py
@@ -10,8 +10,10 @@ from random import choice
 from typing import Set
 from queue import PriorityQueue
 
-queues = PriorityQueue(50001)
+queues = {}
 excluded = set()
+working_q = []
+
 
 def my_rand(start: int, end: int, exclude_values: Set[int] = None):
     if not exclude_values: # in this case, there are no values to exclude so there is no point in filtering out any
@@ -38,44 +40,58 @@ class ServerState:
                     
                     if s.endswith("\n"):
                         count = s.count("\n")
+                        #print(count)
                         requests = s.splitlines()
-                        print(f'number of lines {count}')
                         for r in requests:
+                            #print(r)
                             j = json.loads(r)
                             if j['request'] == "get":
                                 q_list =  j['queues']
+                                #print(f'length of que {queues}')
                                 found = False
                                 priority = -1
-                                queue_to_return = None
+                                res = None
                                 for q in q_list:
                                     try:
-                                        l = queues[q]
-                                        if l['pri'] > priority:
-                                            res = {"status":"ok", "pri":l['pri'], "id":l["id"], "job": l["job"], "queue":q}
-                                            priority = l['pri']
-                                            queue_to_return = q
+                                        jobs = queues[q]
+                                        #print(f'queue {q} jobs {jobs}')
+                                        for job in jobs:
+                                            if job['id'] not in working_q:
+                                                #print(f"checking {job['pri']} against priority {priority}")
+                                                if job['pri'] > priority:
+                                                    res = {"status":"ok", "pri":job['pri'], "id":job["id"], "job": job["job"], "queue":q}
+                                                    #print(f'response to be sent {res}')
+                                                    priority = job['pri']
                                     except:
-                                        pass
-                                    if queue_to_return:
-                                        ltr = queues[queue_to_return]
-                                        res = {"status":"ok", "pri":ltr['pri'], "id":ltr["id"], "job": ltr["job"], "queue":queue_to_return}
-                                        writer.write(bytes(json.dumps(res) + "\n", "utf=8"))
-                                        await writer.drain()
+                                        import traceback
+                                        traceback.print_exc()
+                                if res:
+                                    found = True
+                                    #ltr = queues[queue_to_return]
+                                    working_q.append(res['id'])
+                                    #res = {"status":"ok", "pri":ltr['pri'], "id":ltr["id"], "job": ltr["job"], "queue":queue_to_return}
+                                    #print(f'sending back {res}')
+                                    writer.write(bytes(json.dumps(res) + "\n", "utf=8"))
+                                    await writer.drain()
+                                    #print("sent complete")
                                 if found == False:
                                     res = {"status":"no-job"}
                                     writer.write(bytes(json.dumps(res) + "\n", "utf=8"))
                                     await writer.drain()
                                 bufferobj = bytearray()
                             if j['request'] == "put":
-                                unique_id = my_rand(-1000000, 10000000, excluded)
+                                unique_id = my_rand(1, 500001, excluded)
                                 excluded.add(unique_id)
-                                queues[j['queue']] = {"job":j['job'], "pri": j["pri"], "id":12345}
+                                if j['queue'] not in queues:
+                                    queues[j['queue']] = [{"job":j['job'], "pri": j["pri"], "id":unique_id}]
+                                else:
+                                    queues[j['queue']].append({"job":j['job'], "pri": j["pri"], "id":unique_id})
                                 res = {"status":"ok","id":unique_id}
                                 writer.write(bytes(json.dumps(res) + "\n", "utf=8"))
                                 await writer.drain()
                                 bufferobj = bytearray()
-            writer.close()
-            self._writers.remove(writer)
+            #writer.close()
+            #self._writers.remove(writer)
 
         except Exception as e:
             import traceback
